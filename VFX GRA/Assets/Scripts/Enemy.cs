a59bdf895f099634b8eecf37575b705166f95bac
CamHargrave <tvastacarious@gmail.com>
2019-04-07 16:46:46 -0500

    Ai
    The AI moves now

---
+++ VFX GRA/Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Animator anim;
+    public GameObject enemy;
+    
+    public GameObject GetEnemy()
+    {
+        return enemy;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("distance", Vector3.Distance(transform.position, enemy.transform.position));
+    }
+}
---
+++ VFX GRA/Assets/Scripts/NPCBaseFSM.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCBaseFSM : StateMachineBehaviour
+{
+    public GameObject NPC;
+    public GameObject opponent;
+    [SerializeField]
+    public float turnspeed = 2f;
+    [SerializeField]
+    public float movespeed = 2f;
+    [SerializeField]
+    public float closeToWaypoint = 2f;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        NPC = animator.gameObject;
+        opponent = NPC.GetComponent<Enemy>().GetEnemy();
+    }
+
+
+}
