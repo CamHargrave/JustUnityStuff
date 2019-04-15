1d83403d16f35855ea2e6cc2c00932c7278832f4
CamHargrave <tvastacarious@gmail.com>
2019-04-15 15:22:05 -0500

    Commit
    Commit

---
+++ VFX GRA/Assets/Scripts/Grid.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public Transform StartPosition;
+    public LayerMask WallMask;
+    public Vector2 gridWorldSize;
+    public float nodeRadius;
+    public float Distance;
+
+    Node[,] grid;
+    public List<Node> FinalPath;
+
+    float nodeDiameter;
+    int gridSizeX, gridSizeY;
+
+    private void Start()
+    {
+        nodeDiameter = nodeRadius * 2;
+        gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+        CreateGrid();
+    }
+
+    private void CreateGrid()
+    {
+        grid = new Node[gridSizeX, gridSizeY];
+        Vector3 bottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2;
+        for (int y = 0; y < gridSizeX; y++)
+        {
+            for (int x = 0; x < gridSizeX; x++)
+            {
+                Vector3 worldPoint = bottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
+                bool Wall = true;
+
+                if(Physics.CheckSphere(worldPoint, nodeRadius, WallMask))
+                {
+                    Wall = false;
+                }
+
+                grid[y, x] = new Node(Wall, worldPoint, x, y);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
+
+        if(grid != null)
+        {
+            foreach(Node n in grid)
+            {
+                if (n.IsWall)
+                {
+                    Gizmos.color = Color.white;
+                }
+                else
+                {
+                    Gizmos.color = Color.yellow;
+                }
+
+                if(FinalPath != null)
+                {
+                    if (FinalPath.Contains(n))
+                    {
+                        Gizmos.color = Color.red;
+                    }
+                }
+
+                Gizmos.DrawCube(n.position, Vector3.one * (nodeDiameter - Distance));
+            }
+        }
+    }
+}
